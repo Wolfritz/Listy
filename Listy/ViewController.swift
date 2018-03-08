1a3c7be23b3d13bafc2ee602027afd05faf37c30
Gianluca Frison <Gian@air-di-gianluca.homenet.telecomitalia.it>
2018-03-08 17:13:18 +0100

    Initial Commit

---
+++ Listy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listy
+//
+//  Created by Gianluca Frison on 08/03/18.
+//  Copyright © 2018 Gianluca Frison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
